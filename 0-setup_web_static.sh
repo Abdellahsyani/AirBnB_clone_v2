9799bf8df975c6bdca3c90df17d00bbfc0132620
Abdellahsyani <syaniabdos@gmail.com>
2024-04-08 09:47:51 +0000

    fix some error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env bash
-#create a lot of thing that let me dizzy
-#devops is confusing me
-sudo apt update
-sudo apt install nginx -y
+# script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-mkdir -p /data/web_static/{shared,releases/test}
-echo "<html>
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
-old_content='server_name _;'
-new_content='server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}'
-sed -i "s/$old_content/$new_content/" /etc/nginx/sites-available/default
-service nginx --full-restart
-true
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
