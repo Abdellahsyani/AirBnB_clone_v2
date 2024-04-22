288f09e49f13abff5feb78d30ef484c9403240de
Abdellahsyani <syaniabdos@gmail.com>
2024-04-22 19:04:21 +0100

    done

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""this module display multiple pages"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello():
+    """hello method"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """method hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def text(text):
+    """text method"""
+    if '_' in text:
+        text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route("/python/<text>")
+@app.route("/python")
+def python(text="is cool"):
+    """the python method"""
+    if '_' in text:
+        text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route("/number/<n>")
+def number(n):
+    """method to print number"""
+    if isinstance(n, int):
+        return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
