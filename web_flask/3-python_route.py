8a353d4bdbea733396f95c632f3574d7874911f4
Abdellahsyani <syaniabdos@gmail.com>
2024-04-22 17:58:35 +0100

    done

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""this module display a various thing"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello():
+    """hello method"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """method hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def text(text="is cool"):
+    """text method"""
+    if '_' in text:
+        text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route("/python/<text>")
+def python(text="is cool"):
+    """the python method"""
+    if '_' in text:
+        text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
