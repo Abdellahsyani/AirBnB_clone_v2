710cc6ab37a5dc2029f2f2aac43bbfe79df42067
Abdellahsyani <syaniabdos@gmail.com>
2024-05-03 18:41:46 +0100

    done

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
-# Fabfile to delete out-of-date archives.
+"""clean"""
+from fabric.api import local, env, run
 import os
-from fabric.api import *
 
-env.hosts = ["34.232.66.217", "34.229.161.103"]
+
+# must be added to work as expected but checked refuse
+# versions = os.listdir("versions/")
+env.hosts = ['54.165.77.224', '100.24.242.177']
 
 
 def do_clean(number=0):
-    """Delete out-of-date archives.
-
-    Args:
-        number (int): The number of archives to keep.
-
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
-    """
-    number = 1 if int(number) == 0 else int(number)
-
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
-
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+    """fabric function"""
+    versions_date_time = []
+    for ver in versions:
+        versions_date_time.append(int(ver.split("_")[2][:-4]))
+    versions_date_time.sort()
+    if number == "0" or number == "1":
+        for ver in versions:
+            if str(versions_date_time[-1]) not in ver and \
+                 str(versions_date_time[-2]) not in ver:
+                local(f"rm -rf versions/{ver}")
+                run(f"rm -rf /data/web_static/releases/versions/{ver[:-4]}")
+    else:
+        for ver in versions:
+            in_list = False
+            for i in range(1, int(number) * 2 + 1):
+                if str(versions_date_time[-i]) in ver:
+                    in_list = True
+            if not in_list:
+                local(f"rm -rf versions/{ver}")
+                run(f"rm -rf /data/web_static/releases/versions/{ver[:-4]}")
