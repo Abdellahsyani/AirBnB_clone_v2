a4c4c7e34556af62048ff4ea74a6cada59160444
Abdellahsyani <syaniabdos@gmail.com>
2024-05-03 18:31:44 +0100

    done

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,29 @@
-#!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from os import getenv
+import streamlit as st
 
+def page1():
+  """Content and logic for page 1"""
+  name = st.text_input("Enter your name:")
+  submit_button = st.button("Submit")
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
+  if submit_button:
+    st.session_state["user_name"] = name  # Store user input
+    st.success("Information submitted!")
+    st.experimental_rerun()  # Trigger page refresh
+
+# page2.py
+
+import streamlit as st
+
+def page2():
+  """Content and logic for page 2"""
+  if "user_name" in st.session_state:
+    name = st.session_state["user_name"]
+    st.write(f"Welcome, {name}!")
+  else:
+    st.write("Please enter your information on the previous page.")
+
+# Main Script (logic for displaying pages)
+if "user_name" in st.session_state:
+  page2()
 else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-
-storage.reload()
+  page1()
